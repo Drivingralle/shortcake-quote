f72a15f6472351c234cc07a69df0cb3e2f412962
Drivingralle <drivingralle@me.com>
2015-08-05 13:02:25 +0200

    Add Plugin header

---
+++ shortcake-quote.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Plugin Name: Shortcake - Quote
+ * Version: 1.0
+ * Description: Adds [shortcake_quote] shortcode to WordPress to use with Shortcode UI (Shortcake)
+ * Author: Drivingralle
+ * Author URI: http://www.drivingralle.de
+ * Text Domain: shortcake_quote
+ * License: GPL v3 or later
+ *
+ * This plugin is based on the "Shortcode UI Example" by Fusion Engineering and community
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
